a45c100240722f4a7fbeb419d3954e5c90494ff3
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-05-06 12:21:40 +0800

    fix: Fix bug to compile

--- headers/usagecheck.h
+++ headers/usagecheck.h
@@ -4,14 +4,18 @@
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 #include <iomanip>
 #include <vector>
 #include "datapoint.h"
 #include "../src/Eigen/Dense"
 
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
 void check_arguments(int argc, char* argv[]);
-void check_files(ifstream& in_file, string& in_nams, ofstream& out_file, string& out_name);
-void print_EKF_data(const VectorXd &RMSE, const vector<VectorXd> &estimations,
+void check_files(std::ifstream& in_file, std::string& in_names, std::ofstream& out_file, std::string& out_name);
+void print_EKF_data(const VectorXd &RMSE, const std::vector<VectorXd> &estimations,
     const std::vector<VectorXd> &ground_truths, const std::vector<DataPoint> &all_sensor_data);
 
 #endif /* USAGECHECK_H_ */
